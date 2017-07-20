bac048dee4555504a1e49c503978d6f71c4884f4
Maxim Cherkasov <maxim.cherkasov@gmail.com>
2017-07-20 18:39:14 +0300

    IMR-5 Send message to specific chat

--- scripts/testing_frontend.sh
+++ scripts/testing_frontend.sh
@@ -10,7 +10,7 @@ pip3 install --upgrade pip==9.0.1
 
 # Step 3 - Run js tests
 pip3 install nodeenv
-nodeenv --node=6.9.1 --python-virtualenv --prebuilt
+nodeenv --node=7.10.1 --python-virtualenv --prebuilt
 
 deactivate
 . $VENV_PATH/bin/activate
